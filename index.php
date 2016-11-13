bd029f4223cda45284d9807c49e7356d1f8846af
matiasgsuarez <matiasgsuarez@gmail.com>
2016-11-12 21:04:37 -0300

    actualizo

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 define('APPLICATION_NAME', 'Drive API PHP Quickstart');
 define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
 
-$url_success = "http://php-matias-prueba11.44fs.preview.openshiftapps.com/index.php";
+$url_success = "http://php-matias-prueba11.44fs.preview.openshiftapps.com";
 
 $client = new Google_Client();
 $client->setApplicationName(APPLICATION_NAME);
@@ -18,7 +18,7 @@ $client->addScope("https://www.googleapis.com/auth/drive.metadata.readonly");
 $client->addScope("https://www.googleapis.com/auth/drive.metadata");
 $client->addScope("https://www.googleapis.com/auth/drive.photos.readonly");
 $redirect_uri = 'http://127.0.0.1/cloud/login.php';
-$client->setRedirectUri($url_success);
+$client->setRedirectUri($redirect_uri);
 
 if (!isset($_GET['code'])) {
 	// Verifica las credenciales
